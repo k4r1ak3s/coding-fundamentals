e4f187ae4d9fa203edab5a98cfe861d17e94e2a7
karl <karl@pop-os.localdomain>
2024-01-19 12:28:01 +0000

    added lab7

--- lab7.py
+++ lab7.py
@@ -1,9 +1,8 @@
-import csv
 import pandas as pd
 
 car_sales_csv = pd.read_csv('carSale.csv')
 car_sales_df = car_sales_csv.reset_index(drop=True).rename(columns={"Unnamed: 0":"Manufacturer"})
 car_sales_df['Yearly Total'] = car_sales_df.drop('Manufacturer', axis=1).sum(axis=1)
-car_sales_df.loc['Total',:] = car_sales_df.sum(axis=0)
+car_sales_df.loc['Monthly Total',:] = car_sales_df.sum(axis=0)
 
 print(car_sales_df)
